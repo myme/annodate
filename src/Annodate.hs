826e2cd0d1b597f326ab2a3fd2b9bf9bd6501e28
Martin Øinæs Myrseth <myrseth@gmail.com>
2017-11-16 23:55:45 +0100

    Use local time

--- src/Annodate.hs
+++ src/Annodate.hs
@@ -1,13 +1,15 @@
 module Annodate
   ( annotateLine
+  , annotateLineIO
   , annotateIO
   ) where
 
-import  Control.Exception (tryJust)
-import  Control.Monad     (guard)
-import  Data.Time         (FormatTime, defaultTimeLocale, formatTime, getCurrentTime)
-import  GHC.IO.Handle
-import  System.IO.Error   (isEOFError)
+import  Control.Exception   (tryJust)
+import  Control.Monad       (guard)
+import  Data.Time           (FormatTime, defaultTimeLocale, formatTime)
+import  Data.Time.LocalTime (getZonedTime)
+import  GHC.IO.Handle       (Handle, hGetLine)
+import  System.IO.Error     (isEOFError)
 
 type DateFormat = String
 
@@ -15,12 +17,16 @@ annotateLine :: FormatTime t => DateFormat -> t -> String -> String
 annotateLine format time line = timeString ++ ": " ++ line
     where timeString = formatTime defaultTimeLocale format time
 
+annotateLineIO :: DateFormat -> String -> IO ()
+annotateLineIO format line = do
+            time <- getZonedTime
+            putStrLn $ annotateLine format time line
+
 annotateIO :: DateFormat -> Handle -> IO ()
 annotateIO format handle = do
     input <- tryJust (guard . isEOFError) (hGetLine handle)
     case input of
         Left  _    -> return ()
         Right line -> do
-            time <- getCurrentTime
-            putStrLn $ annotateLine format time line
-            annotateIO format handle
+          annotateLineIO format line
+          annotateIO format handle
