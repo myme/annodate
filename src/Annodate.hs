edf7f1463344fdd6948f4c5da770d1b8f9327b17
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-04-11 22:00:22 +0200

    Refactoring
    Separate output handling from inactivity tracking.

--- src/Annodate.hs
+++ src/Annodate.hs
@@ -5,9 +5,9 @@ module Annodate
   ) where
 
 import Control.Applicative ((<|>))
-import Control.Concurrent (threadDelay, killThread, forkIO, newMVar, MVar, readMVar, modifyMVar_)
+import Control.Concurrent (threadDelay, killThread, forkIO, newMVar, modifyMVar_, withMVar)
 import Control.Exception (tryJust, bracket)
-import Control.Monad (guard, when)
+import Control.Monad (guard, forever, when)
 import Data.Text (Text, pack)
 import Data.Text.IO (hGetLine, hPutStr, hPutStrLn)
 import Data.Time (defaultTimeLocale, formatTime)
@@ -39,32 +39,44 @@ printAnnotatedLine format color line output = do
   setColor output Nothing
   hPutStrLn output $ ": " <> line
 
-inactiveTimer :: MVar Int -> Handle -> IO ()
-inactiveTimer inactivity output = do
-  hSetBuffering output NoBuffering
-  threadDelay 1000000
-  modifyMVar_ inactivity $ \i -> do
-    let newVal = i + 1
-    hPutStr output "\r"
-    setColor output (Just White)
-    hPutStr output ("Inactive for " <> pack (formatDuration newVal))
-    pure newVal
-  inactiveTimer inactivity output
-  where formatDuration i | i == 1 = show i <> " second"
-                         | otherwise = show i <> " seconds"
+withInactivity :: IO a -> (Int -> IO ()) -> IO () -> IO a
+withInactivity action onTick onDone = do
+  inactivity <- newMVar 0
+
+  let inactiveTimer = forkIO $ forever $ do
+        threadDelay 1000000
+        modifyMVar_ inactivity $ \i -> do
+          let newVal = i + 1
+          onTick newVal
+          pure newVal
+
+  bracket inactiveTimer killThread $ \_ -> do
+    result <- action
+    withMVar inactivity $ \i ->
+      when (i > 0) onDone
+    pure result
+
+formatDuration :: Int -> String
+formatDuration i | i == 1 = show i <> " second"
+                 | otherwise = show i <> " seconds"
 
 annotateIO :: Options -> Handle -> Handle -> IO ()
 annotateIO opts input output = do
-  inactivity <- newMVar 0
+  let getLine = tryJust (guard . isEOFError) (hGetLine input)
+      onInactive duration = do
+        hPutStr output "\r"
+        setColor output (Just White)
+        hPutStr output ("Inactive for " <> pack (formatDuration duration))
+
   content <- if optsNoCountPause opts
-    then getLine ()
-    else bracket (forkIO $ inactiveTimer inactivity output) killThread getLine
-  inactiveFor <- readMVar inactivity
-  when (inactiveFor > 0) $ hPutStrLn output ""
+    then getLine
+    else do
+      hSetBuffering output NoBuffering
+      withInactivity getLine onInactive (hPutStrLn output "")
+
   case content of
     Left  _    -> return ()
     Right line -> do
       let color = optsColor opts <|> if optsNoColor opts then Nothing else Just Magenta
       printAnnotatedLine (optsFormat opts) color line output
       annotateIO opts input output
-  where getLine _ = tryJust (guard . isEOFError) (hGetLine input)
