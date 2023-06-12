a7189ce194a0d92c4780c5290832f42ded00c98c
Martin Myrseth <mm@myme.no>
2023-06-12 10:15:00 +0200

    Switch to nixpkgs 23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A very basic flake";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
   outputs = { self, nixpkgs }:
   let system = "x86_64-linux";
       overlay = final: prev: {
